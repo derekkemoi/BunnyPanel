8e66494937e0a02a07090b3e1ed22bfa12580d5b
derekkemoi <derekkemoi@gmail.com>
2024-11-13 13:19:50 +0300

    Update Withdraw.js

--- src/screens/app/Withdraw.js
+++ src/screens/app/Withdraw.js
@@ -58,7 +58,7 @@ export default function Withdraw() {
       //   added: true
       // }))
       setProgressDialog(false)
-      navigate("/profile")
+      navigate("/account")
     }, 5000);
 
 
